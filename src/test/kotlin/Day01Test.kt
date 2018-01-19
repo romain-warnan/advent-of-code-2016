9e3c4b31fd8bb555485bc2075233c89ec6588126
Romain Warnan <romain.warnan@gmail.com>
2018-01-19 18:17:32 +0100

    Plus de main, que des tests

--- src/test/kotlin/Day01Test.kt
+++ src/test/kotlin/Day01Test.kt
@@ -1,66 +1,85 @@
+
 import day01.Day01
 import org.junit.Assert
+import org.junit.BeforeClass
 import org.junit.Test
+import java.nio.file.Files
+import java.nio.file.Paths
 
 class Day01Test {
 
+    private val day = Day01()
+
+    companion object {
+        @BeforeClass
+        fun beforeClass() {
+            println("Day 01")
+        }
+    }
+
+    @Test
+    fun part1() {
+        val input = Files.newBufferedReader(Paths.get("src/main/resources/input01")).readLine()
+        val val1 = day.part1(input)
+        Assert.assertEquals(1034, val1)
+        println("Part 1 : $val1")
+    }
+
+    @Test
+    fun part2() {
+        val input = Files.newBufferedReader(Paths.get("src/main/resources/input01")).readLine()
+        val val2 = day.part2(input)
+        Assert.assertEquals(1356, val2)
+        println("Part 2 : $val2")
+    }
+
     @Test
     fun test11() {
-        val day = Day01()
         Assert.assertEquals(3, day.part1("1122"))
     }
 
     @Test
     fun test12() {
-        val day = Day01()
         Assert.assertEquals(3, day.part1("1122"))
     }
 
     @Test
     fun test13() {
-        val day = Day01()
         Assert.assertEquals(4, day.part1("1111"))
     }
 
     @Test
     fun test14() {
-        val day = Day01()
         Assert.assertEquals(0, day.part1("1234"))
     }
 
     @Test
     fun test15() {
-        val day = Day01()
         Assert.assertEquals(9, day.part1("91212129"))
     }
 
     @Test
     fun test21() {
-        val day = Day01()
         Assert.assertEquals(6, day.part2("1212"))
     }
 
     @Test
     fun test22() {
-        val day = Day01()
         Assert.assertEquals(0, day.part2("1221"))
     }
 
     @Test
     fun test23() {
-        val day = Day01()
         Assert.assertEquals(4, day.part2("123425"))
     }
 
     @Test
     fun test24() {
-        val day = Day01()
         Assert.assertEquals(12, day.part2("123123"))
     }
 
     @Test
     fun test25() {
-        val day = Day01()
         Assert.assertEquals(4, day.part2("12131415"))
     }
 }
