ca41f79860b08585653c6f3bb3bee2c440dab4d4
Romain Warnan <romain.warnan@gmail.com>
2018-02-07 18:55:35 +0100

    Day 1, part 1, smooth

--- src/main/kotlin/day01/Day01.kt
+++ src/main/kotlin/day01/Day01.kt
@@ -1,25 +1,56 @@
 package day01
 
+import kotlin.math.abs
+
 class Day01 {
 
     fun part1(input: String): Long {
-        return count(input, 1)
+        val point = Point()
+        input.split(",")
+                .map { s -> s.trim() }
+                .forEach{ s -> point.turnAndMove(s) }
+        return point.manhattanDistance()
     }
 
-    fun part2(input: String): Long {
-        return count(input, input.length / 2)
-    }
+     data class Point(private var x: Long = 0, private var y: Long = 0, private var way: Way = Way.NORTH) {
 
-    private fun count(input: String, step: Int): Long {
-        val chars = input.toCharArray()
-        var count = 0L
-        for ((index, char) in chars.withIndex()) {
-            val next = chars[(index + step) % chars.size]
-            if (char == next) count += charToInt(char)
+        fun turnAndMove (move: String) {
+            turn(Turn.valueOf(move[0].toString()))
+            move(move.substring(1).toLong())
         }
-        return count
+
+        private fun turn (turn: Turn) {
+            way = when (turn) {
+                Turn.L -> when (way) {
+                    Way.NORTH -> Way.WEST
+                    Way.WEST -> Way.SOUTH
+                    Way.SOUTH -> Way.EAST
+                    Way.EAST -> Way.NORTH
+                }
+                Turn.R -> when (way) {
+                    Way.NORTH -> Way.EAST
+                    Way.WEST -> Way.NORTH
+                    Way.SOUTH -> Way.WEST
+                    Way.EAST -> Way.SOUTH
+                }
+            }
+        }
+
+        private fun move (distance: Long) = when (way) {
+            Way.NORTH -> y += distance
+            Way.SOUTH -> y -= distance
+            Way.EAST -> x += distance
+            Way.WEST -> x -= distance
+        }
+
+        fun manhattanDistance() = abs(x) + abs(y)
+
+        enum class Way {
+            NORTH, SOUTH, WEST, EAST
+        }
+
+        enum class Turn {
+            L, R
+        }
     }
-
-
-    private fun charToInt(char: Char) = Integer.valueOf(char.toString())
 }
--- src/test/kotlin/Day01Test.kt
+++ src/test/kotlin/Day01Test.kt
@@ -13,65 +13,28 @@ class Day01Test {
     fun part1() {
         val input = Files.newBufferedReader(Paths.get("src/main/resources/input01")).readLine()
         val val1 = day.part1(input)
-        Assert.assertEquals(1034, val1)
+        Assert.assertEquals(273, val1)
         println("Part 1 : $val1")
     }
 
-    @Test
-    fun part2() {
-        val input = Files.newBufferedReader(Paths.get("src/main/resources/input01")).readLine()
-        val val2 = day.part2(input)
-        Assert.assertEquals(1356, val2)
-        println("Part 2 : $val2")
-    }
+    /*
+    Following R2, L3 leaves you 2 blocks East and 3 blocks North, or 5 blocks away.
+    R2, R2, R2 leaves you 2 blocks due South of your starting position, which is 2 blocks away.
+    R5, L5, R5, R3 leaves you 12 blocks away.
+    */
 
     @Test
     fun test11() {
-        Assert.assertEquals(3, day.part1("1122"))
+        Assert.assertEquals(5, day.part1("R2, L3"))
     }
 
     @Test
     fun test12() {
-        Assert.assertEquals(3, day.part1("1122"))
+        Assert.assertEquals(2, day.part1("R2, R2, R2"))
     }
 
     @Test
     fun test13() {
-        Assert.assertEquals(4, day.part1("1111"))
-    }
-
-    @Test
-    fun test14() {
-        Assert.assertEquals(0, day.part1("1234"))
-    }
-
-    @Test
-    fun test15() {
-        Assert.assertEquals(9, day.part1("91212129"))
-    }
-
-    @Test
-    fun test21() {
-        Assert.assertEquals(6, day.part2("1212"))
-    }
-
-    @Test
-    fun test22() {
-        Assert.assertEquals(0, day.part2("1221"))
-    }
-
-    @Test
-    fun test23() {
-        Assert.assertEquals(4, day.part2("123425"))
-    }
-
-    @Test
-    fun test24() {
-        Assert.assertEquals(12, day.part2("123123"))
-    }
-
-    @Test
-    fun test25() {
-        Assert.assertEquals(4, day.part2("12131415"))
+        Assert.assertEquals(12, day.part1("R5, L5, R5, R3"))
     }
 }
