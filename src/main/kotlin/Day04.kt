149f07c2062c54cc9328f5f129ff8f2620d95aec
Romain Warnan <romain.warnan@gmail.com>
2018-02-10 19:24:14 +0100

    Début du jour 4

--- src/main/kotlin/Day04.kt
+++ src/main/kotlin/Day04.kt
@@ -1,6 +1,24 @@
+import java.io.File
+
 class Day04 {
 
+   private val regex = "(.*)-(\\d+)\\[(.*)]".toRegex()
+
     fun part1(path: String): Long {
+        File(path).bufferedReader().lines()
+            .map { line -> room(line) }
+            .forEach { println(it) }
         return -1
     }
+
+    private fun room(line: String): Room? {
+        val matchResult = regex.matchEntire(line)
+        if (matchResult != null) {
+            val groupValues = matchResult.groupValues
+            return Room(groupValues[1], groupValues[2].toInt(), groupValues[3])
+        }
+        return null
+    }
+
+    data class Room(val encryptedName: String, val sectorID: Int, val checksum: String)
 }
