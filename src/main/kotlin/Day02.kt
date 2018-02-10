6b027cc8e98ea848c2e94a3ba11f8a7b9c0759c9
Romain Warnan <romain.warnan@gmail.com>
2018-02-10 11:03:13 +0100

    Jour 2, partie 1. Propre

--- src/main/kotlin/Day02.kt
+++ src/main/kotlin/Day02.kt
@@ -1,6 +1,60 @@
+import java.io.File
+
 class Day02 {
 
-    fun part1(input: String): Int {
-        return -1
+    private val keypad = arrayOf(
+        arrayOf(1, 2, 3),
+        arrayOf(4, 5, 6),
+        arrayOf(7, 8, 9))
+
+    fun part1(path: String): String {
+        var code = ""
+        val point = Point()
+        File(path).bufferedReader().lines()
+                .forEach {
+                    it.forEach {
+                        point.move(it)
+                    }
+                    code += point.pointValue(keypad)
+                }
+        return code
+    }
+
+    class Point (private var row: Int = 1, private var col: Int = 1) {
+
+        fun move (way: Char) {
+            when(way) {
+                'U' -> row --
+                'R' -> col ++
+                'D' -> row ++
+                'L' -> col --
+            }
+            checkPoint()
+        }
+
+        fun pointValue(keypad: Array<Array<Int>>) = keypad[row][col]
+
+        private fun checkPoint () {
+            checkRow()
+            checkCol()
+        }
+
+        private fun checkRow() {
+            if (row < 0) {
+                row = 0
+            }
+            if (row > 2) {
+                row = 2
+            }
+        }
+
+        private fun checkCol() {
+            if (col < 0) {
+                col = 0
+            }
+            if (col > 2) {
+                col = 2
+            }
+        }
     }
 }
--- src/test/kotlin/Day02Test.kt
+++ src/test/kotlin/Day02Test.kt
@@ -1,6 +1,6 @@
+
 import org.junit.Assert
 import org.junit.Test
-import java.io.File
 
 class Day02Test {
 
@@ -8,14 +8,13 @@ class Day02Test {
 
     @Test
     fun part1() {
-        val input = File("src/main/resources/input02").bufferedReader().readLine()
-        val answer = day.part1(input)
-        // Assert.assertEquals(273, answer)
+        val answer = day.part1("src/main/resources/input02")
+        Assert.assertEquals("45973", answer)
         println("Part 1 : $answer")
     }
 
     @Test
     fun test1() {
-        Assert.assertEquals(1985, day.part1("src/test/resources/input02"))
+        Assert.assertEquals("1985", day.part1("src/test/resources/input02"))
     }
 }
