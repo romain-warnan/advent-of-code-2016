d3e716f27b10b814b0f3902897403318ee1e1f80
Romain Warnan <romain.warnan@gmail.com>
2018-02-11 16:23:05 +0100

    Refactoring jour 6 pour éviter la duplication de code (et tester un peu Kotlin)

--- src/main/kotlin/Day06.kt
+++ src/main/kotlin/Day06.kt
@@ -3,45 +3,36 @@ import java.io.File
 class Day06 {
 
     fun part1(path: String): String {
-        val lines = File(path).bufferedReader().readLines()
-        val n = messageLength(lines)
-        var message = ""
-        for (i in 0 until n) {
-            message += mostCommonLetter(i, lines)
-        }
-        return message
+        return message(path, mostCommonLetter)
     }
 
     fun part2(path: String): String {
+        return message(path, leastCommonLetter)
+    }
+
+    private fun message(path: String, transform: (Map.Entry<Char, Int>) -> Int): String {
         val lines = File(path).bufferedReader().readLines()
         val n = messageLength(lines)
         var message = ""
         for (i in 0 until n) {
-            message += leastCommonLetter(i, lines)
+            message += selectLetter(i, lines, transform)
         }
         return message
     }
 
+    private fun selectLetter(index: Int, lines: List<String>, transform: (Map.Entry<Char, Int>) -> Int): Char {
+        return lines.map { it.toCharArray()[index] }
+            .groupBy { it }
+            .mapValues { it.value.size }
+            .entries
+            .sortedByDescending{ transform(it) }
+            .first()
+            .key
+    }
+
+    private val mostCommonLetter: (Map.Entry<Char, Int>) -> Int = { it.value }
+
+    private val leastCommonLetter: (Map.Entry<Char, Int>) -> Int = { - it.value }
+
     private fun messageLength(lines: List<String>) = lines[0].length
-
-    private fun mostCommonLetter(index: Int, lines: List<String>): Char {
-        return lines.map { it.toCharArray()[index] }
-            .groupBy { it }
-            .mapValues { it.value.size }
-            .entries
-            .sortedByDescending { it.value }
-            .first()
-            .key
-    }
-
-    private fun leastCommonLetter(index: Int, lines: List<String>): Char {
-        return lines.map { it.toCharArray()[index] }
-            .groupBy { it }
-            .mapValues { it.value.size }
-            .entries
-            .sortedBy { it.value }
-            .first()
-            .key
-    }
-
 }
