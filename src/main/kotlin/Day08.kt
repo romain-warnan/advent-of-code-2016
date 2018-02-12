628cc49e6d56720e94629e8bee7f46b2d15cc5ec
Romain Warnan <romain.warnan@insee.fr>
2018-02-12 16:29:39 +0100

    Jour8, début partie 1

--- src/main/kotlin/Day08.kt
+++ src/main/kotlin/Day08.kt
@@ -1,9 +1,53 @@
-import java.io.File
-
 class Day08 {
 
     fun part1(path: String, width: Int = 50, height: Int = 6): Long {
-        return File(path).bufferedReader().lines()
-            .count()
+        val screen = Array(height, {CharArray(width) {'.'}})
+        return (width * height).toLong()
+    }
+
+    private val rectRegex = "rect (\\d+)x(\\d+)".toRegex()
+    private val rectRotateRow = "rotate row y=(\\d+) by (\\d+)".toRegex()
+    private val rectRotateColumn = "rotate column x=(\\d+) by (\\d+)".toRegex()
+
+    fun fromLine(line: String): Operation = when {
+        rectRotateRow.matches(line) -> {
+            val groups = rectRotateRow.matchEntire(line)!!.groupValues
+            RotateCol(groups[1].toInt(), groups[2].toInt())
+        }
+        rectRotateColumn.matches(line) -> {
+            val groups = rectRotateColumn.matchEntire(line)!!.groupValues
+            RotateRow(groups[1].toInt(), groups[2].toInt())
+        }
+        else -> {
+            val groups = rectRegex.matchEntire(line)!!.groupValues
+            Rect(groups[1].toInt(), groups[2].toInt())
+        }
+    }
+
+    interface Operation {
+        fun apply(screen: Array<CharArray>)
+    }
+
+
+    data class Rect (private val width: Int, private val height: Int): Operation {
+        override fun apply(screen: Array<CharArray>) {
+            for (x in 0 until width) {
+                for (y in 0 until height) {
+                    screen[y][x] = '#'
+                }
+            }
+        }
+    }
+
+    data class RotateCol (private val row: Int, private val step: Int): Operation {
+        override fun apply(screen: Array<CharArray>) {
+            // TODO Code de apply dans RotateCol
+        }
+    }
+
+    data class RotateRow (private val column: Int, private val step: Int): Operation {
+        override fun apply(screen: Array<CharArray>) {
+            // TODO Code de apply dans RotateRow
+        }
     }
 }
