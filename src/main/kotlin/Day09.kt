7f96915ddcf52f447c0b8ef03360c620a6b7c04e
Romain Warnan <romain.warnan@insee.fr>
2018-02-13 16:38:31 +0100

    Jour9, début

--- src/main/kotlin/Day09.kt
+++ src/main/kotlin/Day09.kt
@@ -1,11 +1,16 @@
 class Day09 {
 
-    val regex = "\\((\\d+)x(\\d+)\\)".toRegex()
+    private val regex = "\\((\\d+)x(\\d+)\\)".toRegex()
 
     fun part1(input: String): Int {
+        var decompressedLength = 0
         for(indice in input.indices) {
             if (input[indice] == '(') {
-                val (a, b) = regex.find(input, indice)!!.destructured
+                val (length, times) = regex.find(input, indice)!!.destructured
+
+            }
+            else {
+                decompressedLength ++
             }
         }
         return -1
