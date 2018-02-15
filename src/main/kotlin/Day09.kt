ec5f1516f22317eaf500db048434d6171290009d
Romain Warnan <romain.warnan@insee.fr>
2018-02-15 16:15:52 +0100

    Fourvoyage jour 9

--- src/main/kotlin/Day09.kt
+++ src/main/kotlin/Day09.kt
@@ -1,6 +1,7 @@
 class Day09 {
 
     private val regex = "\\((\\d+)x(\\d+)\\)".toRegex()
+    private val regex2 = "([^A-Z]+)([A-Z]+)".toRegex()
 
     fun part1(input: String): Int {
         var decompressedLength = 0
@@ -21,6 +22,35 @@ class Day09 {
     }
 
     fun part2(input: String): Int {
-        return -1
+        var decompressedLength = 0
+        var indice = 0
+        while (indice < input.length) {
+            if (input[indice] == '(') {
+                val (markers, letters) = regex2.find(input, indice)!!.destructured
+                decompressedLength += partialLength(markers, letters)
+                indice += markers.length + letters.length
+            } else {
+                decompressedLength++
+                indice++
+            }
+        }
+        return decompressedLength
+    }
+
+    private fun partialLength(markers: String, letters: String): Int {
+        val all = regex.findAll(markers)
+        println(markers + " " + letters + " : " + all.map { it.groupValues[1] + "x" + it.groupValues[2] }.toList())
+        val lastLength = all.map { it.groupValues[1].toInt() }.last()
+        val totalTimes = all.map { it.groupValues[2].toInt() }.reduce { a, b -> a * b }
+        return totalTimes * lastLength + letters.substring(lastLength).length
+    }
+
+
+    /**
+     * Cas (3x5)ABCD
+     * */
+    fun a(input: String): Int {
+        val (length, times, letters) = "\\((\\d+)x(\\d+)\\)([A-Z]+)".toRegex().find(input)!!.destructured
+        return length.toInt() * times.toInt() + letters.length - length.toInt()
     }
 }
--- src/test/kotlin/Day09Test.kt
+++ src/test/kotlin/Day09Test.kt
@@ -56,24 +56,35 @@ class Day09Test {
 
     @Test
     fun test21() {
-        Assert.assertEquals("XYZXYZXYZ".length, day.part1("(3x3)XYZ"))
+        Assert.assertEquals("XYZXYZXYZ".length, day.part2("(3x3)XYZ"))
     }
 
 
     @Test
     fun test22() {
-        Assert.assertEquals("XABCABCABCABCABCABCY".length, day.part1("X(8x2)(3x3)ABCY"))
+        Assert.assertEquals("XABCABCABCABCABCABCY".length, day.part2("X(8x2)(3x3)ABCY"))
     }
 
 
     @Test
     fun test23() {
-        Assert.assertEquals(241920, day.part1("(27x12)(20x12)(13x14)(7x10)(1x12)A"))
+        Assert.assertEquals(241920, day.part2("(27x12)(20x12)(13x14)(7x10)(1x12)A"))
     }
 
 
+    /*
+     * (25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN
+     * (3x9)ABC(2x9)XY(5x6)PQRST X (3x18)TWO(5x63)SEVEN
+     * 27 + 18 + 30 + 1 + 54 + 315
+     * */
     @Test
     fun test24() {
-        Assert.assertEquals(445, day.part1("(25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN"))
+        Assert.assertEquals(445, day.part2("(25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN"))
+    }
+
+    @Test
+    fun test25() {
+        Assert.assertEquals(17, day.a("(3x5)ABCDE"))
     }
 }
+
