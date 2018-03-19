61e11d5a2e636190527e907cb51b2dc2d85e15c6
Romain Warnan <romain.warnan@insee.fr>
2018-03-19 18:08:34 +0100

    Jour 18 partie 1. Naif.

--- src/main/kotlin/Day18.kt
+++ src/main/kotlin/Day18.kt
@@ -1,6 +1,35 @@
+import java.io.File
+
 class Day18 {
 
-    fun part1(path: String): Int {
-        return 0
+    fun part1(path: String, rows: Int): Int {
+        val line = File(path).bufferedReader().readLine()
+        var safe = line.map { when (it) {
+            '.' -> true
+            else -> false
+        }}
+        val safes = mutableListOf(safe);
+        repeat(rows - 1) {
+            val nextSafe = mutableListOf<Boolean>()
+            for(i in safe.indices) {
+                val left = if(i == 0) true else safe[i - 1]
+                val center = safe[i]
+                val right = if(i == safe.lastIndex) true else safe[i + 1]
+                nextSafe.add(!isTrap(left, center, right))
+            }
+            safes.add(nextSafe)
+            safe = nextSafe
+        }
+        return safes
+            .flatMap { it }
+            .count { it }
+    }
+
+    private fun isTrap(left: Boolean, center: Boolean, right: Boolean) = when {
+        !left && !center && right -> true
+        !center && !right && left -> true
+        !left && center && right -> true
+        left && center && !right -> true
+        else -> false
     }
 }
--- src/test/kotlin/Day18Test.kt
+++ src/test/kotlin/Day18Test.kt
@@ -7,13 +7,13 @@ class Day18Test {
 
     @Test
     fun part1() {
-        val answer = day.part1("src/main/resources/input15")
+        val answer = day.part1("src/main/resources/input18", 40)
         println("Part 1 : $answer")
-        Assert.assertEquals(-1, answer)
+        Assert.assertEquals(2013, answer)
     }
 
     @Test
     fun test1() {
-         Assert.assertEquals(38, day.part1("src/test/resources/input18"))
+         Assert.assertEquals(38, day.part1("src/test/resources/input18", 10))
     }
 }
