7aaec5e01d7f2d8f6d926062921c78622614eb40
Romain Warnan <romain.warnan@gmail.com>
2018-03-21 14:57:37 +0100

    Jour 19 refactoring : récursion

--- src/main/kotlin/Day19.kt
+++ src/main/kotlin/Day19.kt
@@ -1,17 +1,14 @@
 class Day19 {
 
     fun part1(elfNumber: Int): Int {
-        var elfes = (1..elfNumber).toList()
-        while (elfes.size > 1) {
-            elfes = if (elfes.size.isEven()) {
-                elfes.filterIndexed { index, _ -> index.isEven() }
-            } else {
-                elfes
-                    .filterIndexed { index, _ -> index.isEven() }
-                    .filterIndexed { index, _ -> index > 0 }
-            }
-        }
-        return elfes.first()
+        val elfes = (1..elfNumber).toList()
+        return filter(elfes)
+    }
+
+    private fun filter(elfes: List<Int>): Int = when {
+        elfes.size == 1 -> elfes.first()
+        elfes.size.isEven() -> filter(elfes.filterIndexed { index, _ -> index.isEven() })
+        else -> filter(elfes.filterIndexed { index, _ -> index.isEven() && index > 0 })
     }
 
     private fun Int.isEven() = this % 2 == 0
