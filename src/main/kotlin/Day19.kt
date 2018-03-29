470265cbc83ed559d2424b9ac1286f35bd6bcdda
Romain Warnan <romain.warnan@insee.fr>
2018-03-29 09:08:19 +0200

    Jour 19 partie 2. Renommage. Toujours faux.

--- src/main/kotlin/Day19.kt
+++ src/main/kotlin/Day19.kt
@@ -1,35 +1,33 @@
 class Day19 {
 
     fun part1(elfNumber: Int): Int {
-        val elfes = (1..elfNumber).toList()
-        return filter1(elfes)
+        val elfs = (1..elfNumber).toList()
+        return filter1(elfs)
     }
 
     fun part2(elfNumber: Int): Int {
-        val elfes = (1..elfNumber).toMutableList()
-        return filter2(elfes)
+        val elfs = (1..elfNumber).toMutableList()
+        return filter2(elfs)
     }
 
-    private fun filter1(elfes: List<Int>): Int = when {
-        elfes.size == 1 -> elfes.first()
-        elfes.size.isEven() -> filter1(elfes.filterIndexed { index, _ -> index.isEven() })
-        else -> filter1(elfes.filterIndexed { index, _ -> index.isEven() && index > 0 })
+    private fun filter1(elfs: List<Int>): Int = when {
+        elfs.size == 1 -> elfs.first()
+        elfs.size.isEven() -> filter1(elfs.filterIndexed { index, _ -> index.isEven() })
+        else -> filter1(elfs.filterIndexed { index, _ -> index.isEven() && index > 0 })
     }
 
-    private fun filter2(elfes: MutableList<Int>): Int {
+    private fun filter2(elfs: MutableList<Int>): Int {
         var index = 0
-        while(elfes.size > 1) {
-            val oppositeIndex = oppositeIndex(index, elfes)
-            elfes.removeAt(oppositeIndex)
+        while(elfs.size > 1) {
+            val oppositeIndex = oppositeIndex(index, elfs)
+            elfs.removeAt(oppositeIndex)
             index ++
-            if(index >= elfes.size) index = 0
+            if(index >= elfs.size) index = 0
         }
-        return elfes.first()
+        return elfs.first()
     }
 
-    private fun oppositeIndex(index: Int, elfes: MutableList<Int>): Int {
-        return (index + (elfes.size / 2)) % elfes.size
-    }
+    private fun oppositeIndex(index: Int, elfs: MutableList<Int>) = index + (elfs.size / 2) % elfs.size
 
     private fun Int.isEven() = this % 2 == 0
 }
