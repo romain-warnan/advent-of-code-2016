b821034c3ce6d8da2a9aaf1584d1872d7b6e3646
Romain Warnan <romain.warnan@insee.fr>
2018-11-22 15:59:33 +0100

    Premiers dévs jour 24

--- src/main/kotlin/fr/aoc/Day24.kt
+++ src/main/kotlin/fr/aoc/Day24.kt
@@ -28,23 +28,54 @@ class Day24 {
             for (i in points.indices) {
                 val line = this[i]
                 for (j in line.indices) {
-                    if(this[i][j] == number) return Point(i, j, number)
+                    if(this[i][j] == number) return Point(i, j)
                 }
             }
-            return Point(-1, -1, number)
+            return Point(-1, -1)
         }
 
         fun startPoint() = findPoint(0)
+
+        fun paths(startValue: Int, point: Point, paths: MutableList<Path>): List<Path> {
+            if(point.isNumber()) {
+                paths += Path(startValue, point.number(), point.distance)
+            }
+            return paths
+        }
+
+        inner class Point(val i: Int, val j: Int) {
+
+            var distance = 0
+
+            fun isWall() = points[i][j] == '#'
+
+            fun isPath() = points[i][j] == '.'
+
+            fun isNumber() = !isWall() && !isPath()
+
+            fun number() = when {
+                isNumber() -> points[i][j].toInt()
+                else -> -1
+            }
+
+            fun move(way: Way): Point {
+                distance ++
+                return when (way) {
+                    Way.NORTH -> Point(i - 1, j)
+                    Way.SOUTH -> Point(i + 1, j)
+                    Way.EAST -> Point(i, j + 1)
+                    Way.WEST -> Point(i, j - 1)
+                }
+            }
+
+            fun canMove(way: Way) = !move(way).isWall()
+        }
+
     }
 
     inner class Path(val from: Int, val to: Int, val length: Int)
 
-    inner class Point(val x: Int, val y: Int, val value: Char) {
-
-        fun isWall() = value == '#'
-
-        fun isPath() = value == '.'
-
-        fun isNumber() = !isWall() && !isPath()
+    enum class Way {
+        NORTH, SOUTH, WEST, EAST
     }
 }
